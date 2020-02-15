27e0b2dce78c8fbced446ec6a22d6bc1422ac982
kindfamily <kindfamily@kakao.com>
2020-02-15 10:35:38 +0900

    update:20200215

--- 000_base/sample/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import sample
-
-app_name = 'sample'
-
-urlpatterns = [
-    path('', sample, name='sample_list')
-]
--- 000_base/sample/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Sample
-
-def sample(request):
-    sample = Sample.objects.all()
-    return render(request, 'sample/index.html', {
-        'samples' : sample,
-    })
